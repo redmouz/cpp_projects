0d81130409866a22066a89f9708ee4e644e343e8
Administrateur <Administrateur@DESKTOP-J5A3P7L>
2017-07-21 10:00:19 +0200

    pushing model

---
+++ SDE/src/modele.cpp
@@ -0,0 +1,18 @@
+/*
+ * modele.cpp
+ *
+ *  Created on: 20 juil. 2017
+ *      Author: Administrateur
+ */
+
+#include "modele.h"
+
+modele::modele() {
+	// TODO Auto-generated constructor stub
+
+}
+
+modele::~modele() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SDE/src/modele.h
@@ -0,0 +1,17 @@
+/*
+ * modele.h
+ *
+ *  Created on: 20 juil. 2017
+ *      Author: Administrateur
+ */
+
+#ifndef MODELE_H_
+#define MODELE_H_
+
+class modele {
+public:
+	modele();
+	virtual ~modele();
+};
+
+#endif /* MODELE_H_ */
